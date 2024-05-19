676673464c0797d535a17a0c1fcf8f8fa9a4d394
BdoRosales <166651046+BdoRosales@users.noreply.github.com>
2024-05-18 22:57:40 -0600

    Add files via upload

---
+++ Alquiler.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Act18.Modelos
+{
+    public class Alquiler
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Debe especificar la película alquilada.")]
+        public int PeliculaId { get; set; }
+
+        [Required(ErrorMessage = "Debe especificar el cliente que alquila la película.")]
+        public int ClienteId { get; set; }
+
+        [Required(ErrorMessage = "La fecha de alquiler es requerida.")]
+        public DateTime FechaAlquiler { get; set; }
+
+        [Required(ErrorMessage = "La fecha de devolucion es requerida.")]
+        public DateTime FechaDevolucion { get; set; }
+
+        [Required(ErrorMessage = "El monto es requerida.")]
+        public decimal Monto { get; set; }
+
+        [Required(ErrorMessage = "Debe especificar el estado del alquiler.")]
+        public string? EstadoAlquiler { get; set; }
+
+        virtual public Pelicula? Pelicula { get; set; }
+        virtual public Cliente? Cliente { get; set; }
+
+    }
+}
---
+++ AlquilerDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Act18.Modelos;
+using Microsoft.EntityFrameworkCore;
+
+namespace Act18.Data
+{
+    public class AlquilerDBContext : DbContext
+    {
+        public AlquilerDBContext(DbContextOptions<AlquilerDBContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Pelicula> Peliculas { get; set; }
+        public DbSet<Cliente> Clientes { get; set; }
+        public DbSet<Alquiler> Alquileres { get; set; }
+    }
+}
